513f1e79a6cb454cdf856a6e5fb75decfefbb951
Aidan Laing <aidan@freshworks.io>
2018-02-13 15:16:59 -0800

    Null Token Expiry
    Now returns expired if expired is null

--- mobileauthentication/src/main/java/ca/bc/gov/mobileauthentication/data/models/Token.kt
+++ mobileauthentication/src/main/java/ca/bc/gov/mobileauthentication/data/models/Token.kt
@@ -15,12 +15,18 @@ data class Token(
         @SerializedName("id_token") val idToken: String?,
         @SerializedName("not-before-policy") val notBeforePolicy: Long?,
         @SerializedName("session_state") val sessionState: String?,
-        @SerializedName("expires_at") val expiresAt: Long = System.currentTimeMillis() + ((expiresIn ?: 0) * 1000),
-        @SerializedName("refresh_expires_at") val refreshExpiresAt: Long = System.currentTimeMillis() + ((refreshExpiresIn ?: 0) * 1000)
+        @SerializedName("expires_at") val expiresAt: Long? = System.currentTimeMillis() + ((expiresIn ?: 0) * 1000),
+        @SerializedName("refresh_expires_at") val refreshExpiresAt: Long? = System.currentTimeMillis() + ((refreshExpiresIn ?: 0) * 1000)
 ) {
 
-    fun isExpired(currentTime: Long = System.currentTimeMillis()): Boolean = expiresAt > currentTime
+    fun isExpired(currentTime: Long = System.currentTimeMillis()): Boolean {
+        if (expiresAt == null) return true
+        return expiresAt > currentTime
+    }
 
-    fun isRefreshExpired(currentTime: Long = System.currentTimeMillis()): Boolean = refreshExpiresAt > currentTime
+    fun isRefreshExpired(currentTime: Long = System.currentTimeMillis()): Boolean {
+        if (refreshExpiresAt == null) return true
+        return refreshExpiresAt > currentTime
+    }
 
 }
