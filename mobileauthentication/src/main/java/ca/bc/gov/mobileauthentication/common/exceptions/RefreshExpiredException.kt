cb513e67fbb6d5a18be909866f5cda437ee501d0
Aidan Laing <aidan@freshworks.io>
2018-02-14 10:24:09 -0800

    Apache 2.0 License Headers
    Added license headers to all Kotlin classes

--- mobileauthentication/src/main/java/ca/bc/gov/mobileauthentication/common/exceptions/RefreshExpiredException.kt
+++ mobileauthentication/src/main/java/ca/bc/gov/mobileauthentication/common/exceptions/RefreshExpiredException.kt
@@ -1,8 +1,21 @@
 package ca.bc.gov.mobileauthentication.common.exceptions
 
 /**
- * Created by Aidan Laing on 2018-01-31.
- * Thrown when token refresh is called and the refresh is expired.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by Aidan Laing on 2017-12-12.
+ *
  */
 class RefreshExpiredException : Throwable() {
     override val message: String?
